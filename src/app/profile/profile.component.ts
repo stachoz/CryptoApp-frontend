6324d997f1dd0fd1ab82e820cef8fa8ec0502434
stachoz <stasiekzdybel9@gmail.com>
2024-04-05 22:40:37 +0200

    styling user info in profile component

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, createEnvironmentInjector } from '@angular/core';
 import { UserService } from '../_services/user/user.service';
 import { AuthService } from '../_services/auth/auth.service';
+import { User } from '../_models/User';
 
 @Component({
   selector: 'app-profile',
@@ -8,17 +9,16 @@ import { AuthService } from '../_services/auth/auth.service';
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent implements OnInit {
-  email: string = 'init value';
+  user!: User;
 
   constructor(private userService:UserService, private authService:AuthService) {}
-
 
   ngOnInit(): void {
     const credentials = this.authService.userValue;
     if(credentials){
       this.userService.getUserByUsername(credentials.username).subscribe(
         user => {
-          this.email = user[0].email;
+          this.user = user[0];
         }
       );
     }
